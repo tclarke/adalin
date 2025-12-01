19af3106b4ab1f61fa242916417dd56be4bc5d90
Trevor R.H. Clarke <trevor@notcows.com>
2025-11-30 22:21:01 -0500

    fix: frame checksum calculation

--- src/adalin-frame.adb
+++ src/adalin-frame.adb
@@ -1,4 +1,9 @@
 package body Adalin.Frame is
+   function GetPID (Self : Frame) return Byte is
+   begin
+      return Byte (Self.frame_identifier) + Byte (Self.parity) * 2**6;
+   end GetPID;
+
    procedure Calculate_FID_Parity (Self : in out Frame) is
       ID     : constant Bits6 := Self.frame_identifier;
       --  Helper to test bit N (0..5)
@@ -21,5 +26,40 @@ package body Adalin.Frame is
 
       Self.parity := Bits2 ((if P0 then 2 else 0) + (if P1 then 1 else 0));
    end Calculate_FID_Parity;
+
+   procedure Calculate_Data_Checksum (Self : in out Frame;
+                                      mode : Mode_Type) is
+      sum : Integer := 0;
+   begin
+      if mode = Enhanced
+        and then (not (Self.frame_identifier in 16#3C# .. 16#3D#))
+      then
+         sum := Integer (GetPID (Self));
+      end if;
+
+      for I in 1 .. Self.length loop
+         sum := sum + Integer (Self.data (I));
+         if sum > 255 then
+            sum := sum - 255;
+         end if;
+      end loop;
+
+      --  One's complement
+      Self.checksum := not Byte (sum);
+   end Calculate_Data_Checksum;
+
+   procedure SetFrameIdentifier (Self : in out Frame; ID : Bits6) is
+   begin
+      Self.frame_identifier := ID;
+      Calculate_FID_Parity (Self);
+   end SetFrameIdentifier;
+
+   procedure SetData (Self : in out Frame; New_Data : Data_Array;
+      mode : Mode_Type) is
+   begin
+      Self.length := New_Data'Length;
+      Self.data (1 .. New_Data'Length) := New_Data;
+      Calculate_Data_Checksum (Self, mode);
+   end SetData;
 
 end Adalin.Frame;
--- src/adalin-frame.ads
+++ src/adalin-frame.ads
@@ -1,16 +1,39 @@
 package Adalin.Frame is
 
-   type Bits6 is mod 64;
-   for Bits6'Size use 6;
+      type Byte is mod 256;
+      for Byte'Size use 8;
 
-   type Bits2 is mod 4;
-   for Bits2'Size use 2;
+      type Bits6 is mod 64;
+      for Bits6'Size use 6;
 
-   type Frame is record
-      frame_identifier : aliased Bits6;
-      parity           : aliased Bits2;
-   end record;
+      type Bits2 is mod 4;
+      for Bits2'Size use 2;
 
-   procedure Calculate_FID_Parity (Self : in out Frame);
+      type Data_Array is array (Natural range <>) of aliased Byte;
+
+      type Mode_Type is (Classic, Enhanced);
+
+      type Frame is record
+         frame_identifier : aliased Bits6;
+         parity           : aliased Bits2;
+         data             : Data_Array (1 .. 8) := (others => 0);
+         length           : Natural range 1 .. 8;
+         checksum         : aliased Byte;
+      end record;
+
+      function GetPID (Self : Frame) return Byte;
+
+      procedure Calculate_FID_Parity (Self : in out Frame);
+
+      procedure Calculate_Data_Checksum (Self : in out Frame;
+         mode : Mode_Type)
+         with Pre => Self.length >= 1 and then Self.length <= 8;
+
+      procedure SetFrameIdentifier (Self : in out Frame; ID : Bits6)
+      with Pre => ID <= 63;
+
+      procedure SetData (Self : in out Frame; New_Data : Data_Array;
+      mode : Mode_Type)
+      with Pre => New_Data'Length >= 1 and then New_Data'Length <= 8;
 
 end Adalin.Frame;
